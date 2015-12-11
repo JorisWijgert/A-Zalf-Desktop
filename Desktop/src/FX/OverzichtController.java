05301c48686b2b1dde0dc580eccf2bcc86daeb79
Ramon Janssen <rph.janssen@student.fontys.nl>
2015-12-11 09:46:03 +0100

    OverzichtController

---
+++ Desktop/src/FX/OverzichtController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FX;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author Ramon
+ */
+public class OverzichtController {
+    @FXML
+    ListView lvAfspraken;
+    @FXML
+    TextArea taInformatie, taVolgendeAfspraak, taDiagnose;
+    @FXML
+    ImageView ivFotoDichtbij, ivFotoVeraf;
+    @FXML
+    Button btVolgende;
+
+    @FXML
+    public void volgende(){
+        
+    }
+}
